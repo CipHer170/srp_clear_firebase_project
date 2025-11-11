92adacb60a115af09077d983144daff7bc11e402
CipHer170 <105492413+CipHer170@users.noreply.github.com>
2025-11-11 12:24:16 +0500

    firebase_str_added

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import {
   getFirestore,
   getDocs,
   collection,
+  addDoc,
 } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
 let allBooks = [];
 
 const bookTitle = document.getElementById("bookTitle");
 const bookAuthor = document.getElementById("bookAuthor");
 const bookRating = document.getElementById("bookRating");
-const bookGenre = document.getElementById("bookGenre");
+const bookGenre = document.getElementById("bookGenreSelect");
+const bookComments = document.getElementById("bookComments");
+const bookFinishedDate = document.getElementById("bookFinishedDate");
 const saveBookBtn = document.getElementById("saveBookBtn");
 const bookInfo = document.getElementById("bookInfo");
+const countBooks = document.getElementById("countBooks");
 
 // adding firestore
 // Your web app's Firebase configuration
@@ -33,52 +34,90 @@ const db = getFirestore(app);
 
 // getInfo from db
 const getBooksDb = async () => {
+  // rendering data from db
   const myBooks = await getDocs(collection(db, "books"));
+  console.log(myBooks);
+
   myBooks.forEach((books) => {
     const book = books.data();
+    console.log(book);
     const bookDiv = document.createElement("div");
-    bookDiv.innerHTML = ` <h3>${book.title}</h3> <span>
-                <h4>E-mail: ${book.genre}</h4>
-               <h4>Age: ${book.author}</h4>
-               <span class="action_btns">
-              <b id="deleteUserInfo-${books.id}" class="btn delete_btn">Delete</b  utton>
+    bookDiv.innerHTML = ` 
+          <span class="addBook_title"> 
+            <h3>${book.title}</h3> 
+            <p>Rating: ${book.rating}</p>
           </span>
-          </span>
-    `;
+          <span class="addBook_desc">
+               <h4>Author: ${book.author}</h4>
+              <h4>Genre: ${book.genre}</h4>
+              <span class="addBook_userDesc">
+                ${
+                  book.comment
+                    ? ` <p>comment: ${book.comment}</p>`
+                    : `<p> no comments</p>`
+                }
+                ${
+                  book.finishedDate
+                    ? `<p>finished date: ${book.finishedDate}</p>`
+                    : `<p>not finished</p>`
+                }
+              </span>
+              <button class="btn delete_btn">Delete</button>
+          </span>`;
     bookInfo.appendChild(bookDiv);
-    console.log(bookDiv);
+
+    // counter
+    countBooks.innerHTML = `Total books ` + myBooks.size;
   });
 };
 
 // add info to firebase
 
 async function addBooksFrDb(bookDataValue) {
-  await addDoc(collection(db, "library"), bookDataValue);
+  await addDoc(collection(db, "books"), bookDataValue);
 }
+
 // saving data in aaray
-saveBookBtn.onclick = (event) => {
-  event.preventDeafult();
+saveBookBtn.onclick = (e) => {
+  e.preventDefault();
   //   handling added new data on inputs
   const bookTitleValue = bookTitle.value.trim();
   const bookAuthorValue = bookAuthor.value.trim();
   const bookRatingValue = bookRating.value.trim();
-  const bookGenreValue = bookGenre.value;
+  const bookGenreValue = bookGenre.value.trim();
+  const bookCommentsValue = bookComments.value.trim();
+  const bookFinishedDateValue = bookFinishedDate.value;
   // build object with added data
   const bookDataValue = {
+    bookCreated: new Date().toISOString(),
     title: bookTitleValue,
     author: bookAuthorValue,
     rating: bookRatingValue,
     genre: bookGenreValue,
+    comment: bookCommentsValue,
+    finishedDate: bookFinishedDateValue,
   };
 
+  if (
+    !bookTitleValue ||
+    !bookAuthorValue ||
+    !bookGenreValue ||
+    !bookFinishedDateValue ||
+    !bookCommentsValue ||
+    bookRatingValue < 0
+  ) {
+    alert("Check all fields");
+    return;
+  }
   //   adding in array
   allBooks.push(bookDataValue);
-  getBooksDb();
   addBooksFrDb(bookDataValue);
+  getBooksDb();
   //   empty inputs after forming array
   bookTitle.value = "";
   bookAuthor.value = "";
   bookRating.value = "";
   bookGenre.value = "";
+  bookFinishedDate.value = "";
+  bookComments.value = "";
 };
-getBooksDb();
